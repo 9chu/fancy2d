98e46f800b670bd423e43de3d004d899954cf041
FrankHB <frankhb1989@gmail.com>
2015-05-06 10:51:54 +0800

    防止宏定义冲突。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- fancylib/fcyOS/fcySocket.h
+++ fancylib/fcyOS/fcySocket.h
@@ -3,7 +3,9 @@
 /// @brief fancy Socket·â×°
 ////////////////////////////////////////////////////////////////////////////////
 #pragma once
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
+#	define _WINSOCK_DEPRECATED_NO_WARNINGS
+#endif
 #include "../fcyType.h"
 #include "../fcyException.h"
 
